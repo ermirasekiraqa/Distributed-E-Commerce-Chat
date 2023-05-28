8e71ef945b4302b1f85ad8e3cece4585d765f46b
ermirasekiraqa <ermiraa.sekiraqa@gmail.com>
2023-05-28 21:44:38 +0200

    User chat is done.

--- index.js
+++ index.js
@@ -12,59 +12,69 @@ async function main() {
   const connection = await mysql.createConnection({
     host: "localhost",
     port: 3306,
-    database: "testdatabase",
+    database: "distributed-e-commerce-db",
     user: "root",
     password: "",
   });
 
   const httpServer = http.createServer(app);
 
-  app.get("/", (req, res) => {
-    console.log("heree");
-    res.sendFile(__dirname + "/index.html");
-  });
+  // app.get("/", (req, res) => {
+  //   console.log("heree");
+  //   // res.sendFile(__dirname + "/index.html");
+  // });
 
   const ioServer = new Server(httpServer); // socket server
 
   //@ts-ignore
   ioServer.on("connection", (socket) => {
     console.log("Connected!");
+    // console.log(socket);
 
     socket.on("create-room", async (data) => {
       const adminUserId = await getAdminUserId();
-      console.log("admin ", adminUserId);
       if (!adminUserId) {
         socket.disconnect(`Admins are unavailable!`);
       }
       const roomExists = await checkIfRoomWithParticipantsExists(
-        adminUserId ? adminUserId.id : "1",
+        adminUserId.id,
         data.userId
       );
       if (!roomExists) {
-        createChatRoom(adminUserId ? adminUserId.id : "1", data.userId);
+        createChatRoom(adminUserId.id, data.userId);
+        // console.log('dd ', {participant1:roomExists.participant_1_id, participant2: roomExists.participant_2_id})
+        // socket.emit('display-message',JSON.stringify({participant1:roomExists.participant_1_id, participant2: roomExists.participant_2_id}))
       } else {
         //@ts-ignore
-        const messages = await getRoomMessages(roomExists.chatroom_id);
-        console.log("messages ", messages);
-        socket.emit("display-messages", messages);
+        socket.emit('display-message',JSON.stringify({participant1:roomExists.participant_1_id, participant2: roomExists.participant_2_id}))
+        const messages = await getRoomMessages(roomExists.chatRoom_id);
+        socket.emit("display-messages", { messages: messages });
       }
     });
-
+    
     socket.on("message", async (message) => {
+      
       const parsedMessage = JSON.parse(message);
+
+   
+      const getRoom = await checkIfRoomWithParticipantsExists(parsedMessage.creatorId, parsedMessage.receiverId)
+      
+
       const storeMessage = await createMessage(
-        parsedMessage.roomId,
+        getRoom.chatRoom_id,
         parsedMessage.creatorId,
         parsedMessage.receiverId,
         parsedMessage.content
       );
+
       if (storeMessage?.affectedRows) {
-        socket.emit("display-messages", { content: parsedMessage.content });
+        const messages = await getRoomMessages(getRoom.chatRoom_id)
+        socket.emit("display-messages", { messages: messages });
       }
     });
   });
 
-  httpServer.listen(3000, () => console.log("Listening on port 3000"));
+  httpServer.listen(4000, () => console.log("Listening on port 4000"));
 
   async function createChatRoom(participant1, participant2) {
     try {
@@ -79,7 +89,7 @@ async function main() {
 
   async function checkIfRoomWithParticipantsExists(participant1, participant2) {
     const result = await connection.execute(
-      `SELECT chatroom_id from chatrooms WHERE participant_1_id=${participant1} AND participant_2_id=${participant2}`
+      `SELECT * from chatrooms WHERE participant_1_id=${participant1} AND participant_2_id=${participant2} OR participant_1_id=${participant2} AND participant_2_id=${participant1}`
     );
     return result[0][0];
   }
@@ -104,7 +114,7 @@ async function main() {
 		FROM messages AS m
 		JOIN users AS creator ON m.creator_id = creator.id
 		JOIN users AS receiver ON m.receiver_id = receiver.id
-		WHERE m.room_id=${roomId}`
+		WHERE m.room_id=${roomId} ORDER BY created_at ASC;` 
       );
       return result[0];
     } catch (err) {
